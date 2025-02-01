bad0afabb027c4aa100bf4bc6b7563c01e21c76f
UnyongChoi <unyongchoi@gmail.com>
2025-02-01 11:52:54 +0900

    initialize project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
+
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Test'),
+    return MaterialApp(
+      theme: theme,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
       ),
     );
   }
