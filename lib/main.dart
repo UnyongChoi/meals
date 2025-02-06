5440bdbe97411339ab6b0e6776eeca737544a417
UnyongChoi <unyongchoi@gmail.com>
2025-02-06 14:35:00 +0900

    some fixing warnning

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal/screens/categories.dart';
 import 'package:meal/screens/tabs.dart';
 
 final theme = ThemeData(
